5f91486815cc0a2b2404f151fc4f8d166acb5926
Sonali <sonalirathod84@gmail.com>
2021-12-08 12:50:43 -0500

    added new SearchPage

---
+++ src/main/java/pages/SearchPage.java
@@ -0,0 +1,7 @@
+package pages;
+
+public class SearchPage {
+    public void SearchSomething(){
+        System.out.println("I am searching something");
+    }
+}
--- src/test/java/tests/testMe.java
+++ src/test/java/tests/testMe.java
@@ -7,5 +7,7 @@ public class testMe {
 @Test
     public void testy(){
         System.out.println(" i am very testy");
+        System.out.println("I made some changes");
+    System.out.println("I made some changes for you");
     }
 }
